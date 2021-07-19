973bca7fa88c3de5d4ae71ca6ed15b3d198a5785
SimonRess <52467973+SimonRess@users.noreply.github.com>
2021-07-19 17:48:45 +0200

    Add files via upload

---
+++ Scripts/0 Hello World App/ui.R
@@ -0,0 +1,13 @@
+# Creating a fluid page layout
+  # Consists of rows which in turn include columns
+  # Adjust page automatically to the browser dimentsions
+fluidPage( 
+  # Application title
+    titlePanel("My first App"),
+    "Some text",
+    titlePanel("2. title"),
+    "Some text jssj"
+)
+
+
+
